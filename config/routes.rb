966658b2c4b697ad3a553cd29ac15581ec658974
VitaliyTehza <w.tehza@gmail.com>
2018-03-29 21:26:57 +0300

    gem activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  root to: 'pages#index'
   
   devise_for :users
 	
